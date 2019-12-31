14d8cb2ac3c1d1631f44c1e0b5ad8610d0eb9544
philippe woerth <phil67b@gmail.com>
2019-12-31 14:59:50 +0100

    Updates

---
+++ precache-manifest.99fee97dea9c42f6de746b5b59e6c953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8d781dccecb1ca817acbce746f6244",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aee5523d445a1a0e31c5",
+    "url": "/monsters-rolodex/static/css/main.06db8964.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aee5523d445a1a0e31c5",
+    "url": "/monsters-rolodex/static/js/main.6bfeaf26.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
